d515e21c3bec3df9126c6a263933904c12282a3c
negar miralaie <miralaienegar@gmail.com>
2022-05-27 21:27:35 +0430

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Joi = require("joi");
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    familyName:{
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true    
+    },
+    email:{
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    emailToken:{
+        type: String,
+    },
+    isVerified:{
+        type: String,
+        default: false,
+    },
+    date:{
+        type: Date,
+        default: Date.now()
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+const validate = user =>{
+    const schema = Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().min(7).required().strict(),
+        // date: Joi.date().timestamp().required(),
+    });
+
+    return schema.validate(user);
+}
+
+module.exports = {
+    User,
+    validate,
+};
