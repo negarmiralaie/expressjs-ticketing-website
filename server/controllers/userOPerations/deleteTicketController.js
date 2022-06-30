595dccb82bd8bd0651fc741e54cc55576d2f9159
negar miralaie <miralaienegar@gmail.com>
2022-06-30 17:43:26 +0430

    completed delete ticket

--- server/controllers/userOPerations/deleteTicketController.js
+++ server/controllers/userOPerations/deleteTicketController.js
@@ -1,4 +1,5 @@
 const TicketModel = require('../../models/Ticket');
+const UserModel = require('../../models/User');
 const ObjectId = require('mongodb').ObjectID;
 
 class deleteTicketController {
@@ -7,7 +8,13 @@ class deleteTicketController {
         try{
 
             const ticketId = req.params.ticketId;
+            const user = await TicketModel.find({"_id": ObjectId(ticketId)})
+            // First delete ticket from tickets db
             await TicketModel.deleteOne({_id: ObjectId(ticketId)});
+
+            // Then delete ticket from user tickets arr in db
+            const foundUser = await UserModel.find({"_id": ObjectId(user[0].user)});
+            await UserModel.findOneAndUpdate({_id: ObjectId(user[0].user)},{$pull:{tickets: ObjectId(ticketId)}})
 
             return res.status(200).json({message: "تیکت با موفقیت حذف شد."});
         } catch(error){
