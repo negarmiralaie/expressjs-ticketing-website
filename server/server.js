9aa0d0aa8f3125af46f598bdbd0002472f12025b
negar miralaie <miralaienegar@gmail.com>
2022-07-05 00:57:21 +0430

    deleted some extra comments

--- server/server.js
+++ server/server.js
@@ -18,11 +18,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 };
 
-// &VIEW ENGINE
-// app.set('view engine', 'ejs');
-// app.set('views', __dirname + '/views');
-// app.engine('ejs', require('ejs').__express);
-
 // *Imports all of the routes from ./routes/index.js
 const routes = require('./routes/index');
 app.use(routes);
@@ -32,39 +27,8 @@ require('dotenv').config();
 connectDB();
 
 app.use(async(req, res, next)=>{
-  // const error = new Error("Not found");
-  // error.status = 404
-  // next(error);
   next(createError.NotFound('This route does not exist'));
 });
-
-// Whenever you call next(error) this func will execute.
-// app.use((err, req, res, next) =>{
-//     res.status(err.status || 500);
-//     res.send({
-//         error:{
-//             status: err.status || 500,
-//             message: err.message
-//         }
-//     })
-// })
-
-
-// app.use(async (req, res, next) => {
-//   const error = new Error('Not found')
-//   error.status = 404
-//   next(error);
-// });
-
-// app.use((err, req, res, next) => {
-//   res.status(err.status || 500)
-//   res.send({
-//     error:{
-//       status: err.status || 500,
-//       message: err.message,
-//     }
-//   })
-// })
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
