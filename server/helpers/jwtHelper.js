5d50b370c2b5f42691ac491b4a4f746d293d15b8
negar miralaie <miralaienegar@gmail.com>
2022-07-06 18:32:24 +0430

    added signRefreshToken

--- server/helpers/jwtHelper.js
+++ server/helpers/jwtHelper.js
@@ -13,13 +13,30 @@ module.exports = {
             // We can also put exp and iss in payoad but if we put them in both places we will get an error
             JWT.sign(payload, secret, options, (err, token) => {
                 if (err) {
-                    const errorMessage = err.name === 'JsonWebTokenError' ? 'Unauthorized' : err.message ;
-                    return next(createError.Unauthorized(errorMessage));
-                    // reject(createError.InternalServerError(err));
+                    console.log(err.message);
+                    reject(createError.InternalServerError(err));
                 }
-                req.payload = payload;
-                next();
-                // resolve(token);
+                resolve(token);
+            })
+        })
+    },
+    signRefreshToken: (userId) => {
+        return new Promise((resolve, reject) => {
+            const payload = { userId };
+            const secret = process.env.REFRESH_TOKEN_SECRET;
+            const options = {
+                expiresIn: "1y",
+                issuer: "http://localhost:5000",
+            };
+            // We can also put exp and iss in payoad but if we put them in both places we will get an error
+            JWT.sign(payload, secret, options, (err, token) => {
+                if (err) {
+                    console.log(err.message);
+                    // const errorMessage = err.name === 'JsonWebTokenError' ? 'Unauthorized' : err.message ;
+                    // return next(createError.Unauthorized(errorMessage));
+                    reject(createError.InternalServerError(err));
+                }
+                resolve(token);
             })
         })
     }
