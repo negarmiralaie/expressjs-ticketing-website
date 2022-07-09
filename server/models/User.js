9b775d75c2e9d97ed615031038176879f7910755
negar miralaie <miralaienegar@gmail.com>
2022-07-09 22:56:25 +0430

    fixed User schema -> eslint

--- server/models/User.js
+++ server/models/User.js
@@ -1,77 +1,76 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
 const bcrypt = require('bcrypt');
+const createError = require("http-errors");
 
-const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        maxlength: 32,
-        trim: true
-    },
-    familyName: {
-        type: String,
-        required: true,
-        maxlength: 32,
-        trim: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-        lowercase: true,
-        length: 11,
-        unique: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 8,
-    },
-    isVerified: {
-        type: String,
-        default: false,
-    },
-    date: {
-        type: Date,
-        default: Date.now()
-    },
-    verificationId: {
-        type: String
-    },
-    roles: [{
-        type: Array,
-        default: "user"
-    }],
-    tickets: [{
-        type: mongoose.Types.ObjectId,
-        ref: "ticket"
-    }]
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  familyName: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    lowercase: true,
+    length: 11,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  isVerified: {
+    type: String,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  },
+  verificationId: {
+    type: String,
+  },
+  roles: [{
+    type: Array,
+    default: 'user',
+  }],
+  tickets: [{
+    type: mongoose.Types.ObjectId,
+    ref: 'ticket',
+  }],
 });
 
-UserSchema.pre("save", async function (next) {
-    try {
-        if (this.isModified("password")) {
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(this.password, salt);
-            this.password = hashedPassword;
-            next();
-        }
-        next();
-    } catch (error) {
-        next(error);
+UserSchema.pre('save', async function (next) {
+  try {
+    if (this.isModified('password')) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+      next();
     }
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
-UserSchema.methods = {
-    isValidPassword: async function (password) {
-        try {
-            return await bcrypt.compare(password, this.password);
-        } catch (error) {
-            throw error;
-        }
-    }
-}
+UserSchema.methods.isValidPassword = async function isValidPassword(password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    return createError.BadRequest();
+  }
+};
 
 const User = mongoose.model('User', UserSchema);
 
