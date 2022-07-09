acb5a5446d765fdf46834eea57ffe1865714838d
negar miralaie <miralaienegar@gmail.com>
2022-07-10 00:51:24 +0430

    fixed get user tickets controller -> eslint

--- server/controllers/userOPerations/getUserTicketsController.js
+++ server/controllers/userOPerations/getUserTicketsController.js
@@ -1,42 +1,42 @@
+const createError = require('http-errors');
+const ObjectId = require('mongodb').ObjectID;
 const TicketModel = require('../../models/Ticket');
 const UserModel = require('../../models/User');
-const ObjectId = require('mongodb').ObjectID;
-const createError = require("http-errors");
 
-class getUserTicketsController {
-    handleGetUserTickets = async (req, res) => {
+class GetUserTicketsController {
+  handleGetUserTickets = async (req, res, next) => {
+    try {
+      const { userId } = this.req;
+      // Now find user with given id
+      const foundUser = await UserModel.find({
+        _id: ObjectId(userId),
+      });
 
-        try {
-            const userId = this.req.userId;
-            // Now find user with given id
-            const foundUser = await UserModel.find({
-                "_id": ObjectId(userId)
-            })
+      if (!foundUser) throw createError.BadRequest('کاربر موجود نمیباشد');
+      // Use toString for converting "new ObjectId to plain id"
+      const foundUserTicketIds = await foundUser[0].tickets
+        .map((ticketObjectId) => ticketObjectId.toString());
 
-            if (!foundUser) throw createError.BadRequest("کاربر موجود نمیباشد")
-            // Use toString for converting "new ObjectId to plain id"
-            const foundUserTicketIds = await foundUser[0].tickets.map(ticketObjectId => ticketObjectId.toString());
+      const arr = [];
 
-            const arr = [];
+      for (let i = 0; i < foundUserTicketIds.length; i++) {
+        const id = foundUserTicketIds[i];
+        const ticket = TicketModel.find({
+          _id: ObjectId(id),
+        });
+        arr.push(ticket);
+      }
 
-            for (let i = 0; i < foundUserTicketIds.length; i++) {
-                const id = foundUserTicketIds[i];
-                const ticket = await TicketModel.find({
-                    "_id": ObjectId(id)
-                })
-                arr.push(ticket);
-            }
-
-            return res.status(200).json({
-                data: {
-                    arr
-                },
-                message: "تیکت ها با موفقیت دریافت شدند."
-            });
-        } catch (error) {
-            next(error);
-        }
+      return res.status(200).json({
+        data: {
+          arr,
+        },
+        message: 'تیکت ها با موفقیت دریافت شدند.',
+      });
+    } catch (error) {
+      return next(error);
     }
+  };
 }
 
-module.exports = new getUserTicketsController();
+module.exports = new GetUserTicketsController();
