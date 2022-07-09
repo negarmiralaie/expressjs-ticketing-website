cfcfb390faef143cdadfccd5ad9cab4919dd641b
negar miralaie <miralaienegar@gmail.com>
2022-07-09 21:53:03 +0430

    fixed get user tickets -> eslint

--- server/routes/user/get-user-tickets.js
+++ server/routes/user/get-user-tickets.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const getUserTicketsController = require('../../controllers/userOPerations/getUserTicketsController');
-const verifyAccessToken = require("../../middleware/verifyAccessToken");
+const verifyAccessToken = require('../../middleware/verifyAccessToken');
 
 router.get('/get-user-tickets', verifyAccessToken, getUserTicketsController.handleGetUserTickets);
 
