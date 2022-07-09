9fc79251a46afc292aa30537fd16b494c7c777ae
negar miralaie <miralaienegar@gmail.com>
2022-07-09 23:52:22 +0430

    fixed refresh token controller -> eslint

--- server/controllers/authOperations/refreshTokenController.js
+++ server/controllers/authOperations/refreshTokenController.js
@@ -1,31 +1,30 @@
+const createError = require('http-errors');
 const {
-    signAccessToken,
-    signRefreshToken,
-    verifyRefreshToken
-} = require("../../helpers/jwtHelper");
-const createError = require("http-errors");
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} = require('../../helpers/jwtHelper');
 
-class refreshTokenController {
-    handleRefreshToken = async (req, res, next) => {
-        try {
-            const refreshToken = req.body;
-            if (!refreshToken) throw createError.BadRequest();
-            const userId = await verifyRefreshToken(refreshToken.refreshToken);
-            // If user's refreshToken was valid we should create a new accesToken and a new refreshToken
-            const accessToken = await signAccessToken(userId);
-            const newRefreshToken = await signRefreshToken(userId);
+class RefreshTokenController {
+  handleRefreshToken = async (req, res, next) => {
+    try {
+      const refreshToken = req.body;
+      if (!refreshToken) throw createError.BadRequest();
+      const userId = await verifyRefreshToken(refreshToken.refreshToken);
+      // If user's refreshToken was valid we should create a new accesToken and a new refreshToken
+      const accessToken = await signAccessToken(userId);
+      const newRefreshToken = await signRefreshToken(userId);
 
-            return res.json({
-                data: {
-                    accessToken: accessToken,
-                    refreshToken: newRefreshToken
-                }
-            });
-        } catch (error) {
-            next(error);
-        }
+      return res.json({
+        data: {
+          accessToken,
+          refreshToken: newRefreshToken,
+        },
+      });
+    } catch (error) {
+      return next(error);
     }
-
+  };
 }
 
-module.exports = new refreshTokenController();
+module.exports = new RefreshTokenController();
