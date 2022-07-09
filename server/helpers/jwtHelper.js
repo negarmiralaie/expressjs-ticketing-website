728de23c2eafe0de569eb73c7697c29c8e5ac767
negar miralaie <miralaienegar@gmail.com>
2022-07-10 02:26:40 +0430

    fixed sendSMS -> eslint

--- server/helpers/jwtHelper.js
+++ server/helpers/jwtHelper.js
@@ -1,28 +1,27 @@
+const createError = require('http-errors');
 const JWT = require('jsonwebtoken');
 const client = require('../config/init_redis');
-const createError = require('http-errors');
 
 module.exports = {
-    signAccessToken: (userId) => {
-        return new Promise((resolve, reject) => {
-            const payload = {
-                userId
-            };
-            const secret = process.env.ACCESS_TOKEN_SECRET;
-            const options = {
-                expiresIn: "1h",
-                issuer: "http://localhost:5000",
-            };
-            // We can also put exp and iss in payoad but if we put them in both places we will get an error
-            JWT.sign(payload, secret, options, (err, token) => {
-                if (err) {
-                    console.log(err.message);
-                    reject(createError.InternalServerError(err));
-                }
-                resolve(token);
-            })
-        })
-    },
+  signAccessToken: (userId) => { return new Promise((resolve, reject) => {
+      const payload = {
+        userId,
+      };
+      const secret = process.env.ACCESS_TOKEN_SECRET;
+      const options = {
+        expiresIn: "1h",
+        issuer: "http://localhost:5000",
+      };
+      // We can also put exp and iss in payoad but if we put them in both places we will get an error
+      JWT.sign(payload, secret, options, (err, token) => {
+        if (err) {
+          console.log(err.message);
+          reject(createError.InternalServerError(err));
+        }
+        resolve(token);
+      });
+    });
+  },
     signRefreshToken: async (userId) => {
         return new Promise((resolve, reject) => {
             const payload = {
--- server/helpers/sendSMS.js
+++ server/helpers/sendSMS.js
@@ -1,22 +1,21 @@
-const Ghasedak = require("ghasedak");
+const Ghasedak = require('ghasedak');
 
 const sendSMS = async (phoneNumber, text) => {
   try {
-    let ghasedak = new Ghasedak(
-      "8ba9e78380c0789f01a49ec9b0c7911e9e904f28b98b274d379f3787cbb776cd"
+    const ghasedak = new Ghasedak(
+      process.env.GHASEDAK_API_KEY,
     );
 
     ghasedak.send({
       message: text,
       receptor: phoneNumber,
-      linenumber: "10008566",
+      linenumber: '10008566',
     });
 
     return true;
   } catch (err) {
     return false;
   }
-
-}
+};
 
 module.exports = sendSMS;
