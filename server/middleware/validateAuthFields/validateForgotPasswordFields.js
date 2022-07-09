e0194e1b364236943d867c7196f5e0a8a8ff5445
negar miralaie <miralaienegar@gmail.com>
2022-07-09 21:52:34 +0430

    fixed validate forgot password fields -> eslint

--- server/middleware/validateAuthFields/validateForgotPasswordFields.js
+++ server/middleware/validateAuthFields/validateForgotPasswordFields.js
@@ -1,11 +1,11 @@
 const {
-    check
+  check,
 } = require('express-validator');
 
 exports.validateForgotPasswordFields = [
-    check("phoneNumber")
+  check('phoneNumber')
     .trim()
     .isLength(11)
     .bail()
-    .isMobilePhone()
+    .isMobilePhone(),
 ];
