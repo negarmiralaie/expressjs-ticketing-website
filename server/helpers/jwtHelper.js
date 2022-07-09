ace7c9157b3b034b84735665b03f8e818c26a1eb
negar miralaie <miralaienegar@gmail.com>
2022-07-10 02:29:36 +0430

    fixed validateAuthFields -> eslint

--- server/helpers/jwtHelper.js
+++ server/helpers/jwtHelper.js
@@ -4,9 +4,9 @@ const client = require('../config/init_redis');
 
 module.exports = {
   signAccessToken: (userId) => { return new Promise((resolve, reject) => {
-      const payload = {
-        userId,
-      };
+    const payload = {
+      userId,
+    };
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
         expiresIn: "1h",
