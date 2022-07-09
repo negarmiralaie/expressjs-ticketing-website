dd963d34041f6e0759cdb6468c30a02b67d676fe
negar miralaie <miralaienegar@gmail.com>
2022-07-09 19:11:24 +0430

    deleted some extra codes

--- server/controllers/authOperations/registerController.js
+++ server/controllers/authOperations/registerController.js
@@ -47,10 +47,7 @@ class registerController {
             const userId = user._id.toString();
 
             const accessToken = await signAccessToken(userId);
-            // console.log('user._id', user._id);
             const refreshToken = await signRefreshToken(userId);
-
-            // console.log('user._id', user._id.toString());
 
             console.log('user.verificationId', user.verificationId)
 
