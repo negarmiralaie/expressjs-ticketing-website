263e91474f298929ebc906dc53c5ad67ef44ba49
negar miralaie <miralaienegar@gmail.com>
2022-07-09 21:56:03 +0430

    fixed create ticket -> eslint

--- server/routes/user/create-ticket.js
+++ server/routes/user/create-ticket.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const createTicketController = require('../../controllers/userOPerations/createTicketController');
-const verifyAccessToken = require("../../middleware/verifyAccessToken");
+const verifyAccessToken = require('../../middleware/verifyAccessToken');
 
 router.post('/create-ticket', verifyAccessToken, createTicketController.handleCreateTicket);
 
