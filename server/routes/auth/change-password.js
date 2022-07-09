36deb42e3f216ad24feb9cc20de511a8d0922291
negar miralaie <miralaienegar@gmail.com>
2022-07-09 21:55:41 +0430

    fixed change password -> eslint

--- server/routes/auth/change-password.js
+++ server/routes/auth/change-password.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 const changePasswordController = require('../../controllers/authOperations/changePasswordController');
-const verifyAccessToken = require("../../middleware/verifyAccessToken");
+const verifyAccessToken = require('../../middleware/verifyAccessToken');
 const {
-    validateChangePasswordFields,
-    validate
+  validateChangePasswordFields,
+  validate,
 } = require('../../middleware/validateAuthFields/validateChangePasswordFields');
 
-
-router.post('/change-password',
-    // validateChangePasswordFields, 
-    // validate , 
-    verifyAccessToken,
-    changePasswordController.handleChangePassword
-)
+router.post(
+  '/change-password',
+  // validateChangePasswordFields, 
+  // validate , 
+  verifyAccessToken,
+  changePasswordController.handleChangePassword,
+);
 
 module.exports = router;
