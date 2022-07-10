27ff3bf7a63668e9e0c517ea00328de8d684bb7d
negar miralaie <miralaienegar@gmail.com>
2022-07-11 02:04:22 +0430

    initial swagger setup -> info

--- server/server.js
+++ server/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieparser = require('cookie-parser');
 const createError = require('http-errors');
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
 const connectDB = require('./config/db');
 const routes = require('./routes/index');
 // ! //////////////////// END OF IMPORTS //////////////////////
 
 const app = express();
+const swaggerJSDocs = YAML.load('./api.yaml');
 require('./config/init_redis');
 require('dotenv').config();
 
@@ -25,6 +28,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // *Imports all of the routes from ./routes/index.js
 app.use(routes);
+// app.use('api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJSDocs));
 
 connectDB();
 
